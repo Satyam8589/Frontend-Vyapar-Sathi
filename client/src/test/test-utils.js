cf8f78b3cfda38d547da6410a5ecb46351c063cc
Satyam8589 <satyamkumarsingh9898@gmail.com>
2026-04-11 03:53:55 +0530

    Fix Navbar test by wrapping with StorePageProvider context

--- client/src/test/test-utils.js
+++ client/src/test/test-utils.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React from "react";
+import { render } from "@testing-library/react";
+import { StorePageProvider } from "@/features/store/context/storePageContext";
 
-// Custom render function that can include providers
+// Custom render function that includes providers
 const customRender = (ui, options = {}) => {
-  return render(ui, { ...options })
-}
+  const Wrapper = ({ children }) => (
+    <StorePageProvider>{children}</StorePageProvider>
+  );
+  return render(ui, { wrapper: Wrapper, ...options });
+};
 
 // Re-export everything from React Testing Library
-export * from '@testing-library/react'
-export { customRender as render }
+export * from "@testing-library/react";
+export { customRender as render };
